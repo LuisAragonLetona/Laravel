2ee5611cc1b5a47bcfcc1b056cc15579d50bd385
Luis Alonso Aragón Letona <luisalonsoaragonletona@gmail.com>
2023-09-22 01:00:05 -0600

    JetStream, MVC, Rutas, y Migraciones

---
+++ resources/views/vista2.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>vista2</title>
+</head>
+
+<body>
+    <h1 class="title">
+        @php echo 'Esto es la VISTA 1';
+        @endphp
+    </h1>
+    <br>
+    <h1>El nombre de usuario es: <strong>{{$nombre}}</strong></h1>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\View;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,8 +15,56 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+    return view('vista1', ['nombre' => 'Luis']);
 });
+Route::get('/persona', 'App\Http\Controllers\PersonaController@index');
+Route::get('/inicio', 'App\Http\Controllers\InicioController@index');
+//ejemplo 1 - regresando texto
+Route::get('/texto', function () {
+    return '<h1>esto es un texto de prueba</h1>';
+});
+//ejemplo 2 - con array
+Route::get('/arreglo', function () {
+    // $arreglo = ['lunes', 'martes', 'miercoles'];
+    $arreglo = [
+        'Id' => '1',
+        'Descripcion' => 'Producto 1'
+    ];/* arreglo asociativo */
+    return $arreglo;
+});
+//ejemplo 3 - parámetros
+Route::get('/nombre/{nombre}', function ($nombre) {
+    return '<h1>El nombre es: ' . $nombre . '</h1>';
+});
+//ejemplo 4 - parametros por default
+Route::get('/cliente/{cliente?}', function ($cliente = 'Cliente general') {
+    return '<h1>El Cliente es: ' . $cliente . '</h1>';
+});
+//ejemplo 5 - redireccionamiento por nombre
+Route::get('/ruta1', function () {
+    return '<h1>esta es la vista de la ruta 1</h1>';
+})->name('rutaNro1');
+Route::get('/ruta2', function () {
+    //return '<h1>esta es la vista de la ruta 2</h1>';
+    return redirect()->route('rutaNro1');
+});
+
+//ejemplo 6
+Route::get('/usuario/{usuario}', function ($usuario) {
+    return '<h1>El usuario es: ' . $usuario . '</h1>';
+})->where('usuario', '[0-9]+');
+//})->where('usuario', '[A-Za-z]+');
+
+/* if (View::exists('vista2')) {
+    Route::get('/', function () {
+        return view('vista2');
+    });
+} else {
+    Route::get('/', function () {
+        return 'la vista solicitada no existe';
+    });
+} */
 
 Route::middleware([
     'auth:sanctum',
